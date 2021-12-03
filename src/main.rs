0c475dfe50f8a0c20fdc13ba51813f78cb8525c8
Heitor Alves de Siqueira <halves-git@ctrlbyte.com>
2021-12-03 20:05:10 -0300

    main: re-add modules for each day

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+mod day1;
 mod day2;
+mod day3;
 
 fn main() {
-    println!("part 1: {}", day2::part1());
-    println!("part 2: {}", day2::part2());
+    println!("day1: {} {}",  day1::part1(),  day1::part2());
+    println!("day2: {} {}",  day2::part1(),  day2::part2());
+    println!("day3: {} {}",  day3::part1(),  day3::part2());
 }
